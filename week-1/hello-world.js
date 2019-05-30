3f09816911b4bf85e27b51b6faf8d36274c7fa90
Loren Wetzel <l.wetzel900@gmail.com>
2019-05-29 23:09:24 -0500

    forgot Code Attributions

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,1 +1,10 @@
+/*
+Title: Assignment 1.3
+Author: Loren Wetzel
+Date: 29 May 2019
+Modified By: 
+Description: This is a simple "Hello World"
+js app to demonstrate js and gitHub 
+*/
+
 console.log("Hello World, my name is Loren Wetzel");
