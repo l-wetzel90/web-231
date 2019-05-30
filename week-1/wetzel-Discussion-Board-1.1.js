996b3956d441e2e5c6f34a312099e9b26392d622
Loren Wetzel <l.wetzel900@gmail.com>
2019-05-30 00:13:20 -0500

    added js file for discussion board

---
+++ week-1/wetzel-Discussion-Board-1.1.js
@@ -0,0 +1,24 @@
+/*
+Title: Discussion Board 1.1
+Author: Loren Wetzel
+Date: 29 May 2019
+Modified By: 
+Description: JS app with values and 
+variables. Contains errors.
+*/
+
+/*expected outcome
+
+Life is really simple, but we insist on making it complicated.
+
+Confucius
+*/
+
+//variables
+var a = "Life is really simple,", b = " but we insist on ", c = "making it complicated.", var d = "Confucius";
+
+//combine them into a single string
+var f = a + b + c + "/n/n" + d;
+
+//output
+console.log("f");
