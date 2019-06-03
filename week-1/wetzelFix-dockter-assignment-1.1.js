3d8e2548280cac2949a13381ac4235d9288fc5d4
Loren Wetzel <l.wetzel900@gmail.com>
2019-06-02 23:35:36 -0500

    adding 2 files from discusion board

---
+++ week-1/wetzelFix-dockter-assignment-1.1.js
@@ -0,0 +1,32 @@
+/*
+========================
+; Title: Assignment 1.1
+; Author: Brittany Dockter
+; Date: May 29, 2019
+; Modified by: Loren Wetzel
+; Description: Short error ridden
+; program for my classmates to fix.
+;=======================
+*/
+
+/* Start Program */
+
+/* Expected output: 
+
+* Life
+* Is but
+* A
+* Dream
+
+*/
+
+// Declare variables
+var a = "Life"
+var b = "Is but"
+var c = "A"
+var d = "Dream"
+
+// Output 
+console.log(a + "\n" + b + "\n"+ c + "\n" + d)
+
+// End Program
---
+++ week-1/wetzelFix-vanroekel-assignment-1.1.js
@@ -0,0 +1,36 @@
+/*
+=============================================
+; Title: Assignment 1.1
+; Author: Faye Van Roekel
+; Date: 29 May 2019
+; Modified By: Loren Wetzel
+; Description: This program has errors in a program
+; for my classmates to fix.
+;============================================
+*/
+
+/* Start Program */
+
+//<script>
+
+//html tags not needed in js file unless you are outputting an element.
+//alert('Welcome to the Error Website');
+
+//</script>
+
+/*
+  Expected output:
+    Console out, Karen Kann, 560 First Street Circle, Carnes, IA, 51250
+*/
+
+// Declare Variables
+var name = "Karen Kann";// matching quotes needed here
+var address = "560 First Street Circle";//quotes needed for strings
+var city = "Carnes";
+var state = "Iowa";
+var zip = 51250
+// Output
+//console.log needed and quotes around comma and space needed for proper output.
+console.log(name + ", " + address +", " +  city + ", " + state + ", " + zip);
+/*=============================================
+// End program*/
