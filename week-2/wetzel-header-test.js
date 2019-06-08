b1d276383fb2aadcb2ee943f8b36fcb7c116d5a3
Loren Wetzel <l.wetzel900@gmail.com>
2019-06-08 16:53:12 -0500

    added header.js and week 2 folder with assignment for 2.1

---
+++ week-2/wetzel-header-test.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Loren Wetzel
+; Date:   8 June 2019
+; Description: Displays a formatted header
+;===========================================
+*/
+
+// constructing header
+const header = require('../wetzel-header.js');
+console.log(header.display("Loren","Wetzel","Assignment 2.1"));
