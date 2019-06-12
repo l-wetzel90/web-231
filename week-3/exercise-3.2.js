5c7d2d4bde4cda3b1dba4f32b38b23bd7fb24b43
Loren Wetzel <l.wetzel900@gmail.com>
2019-06-12 17:29:28 -0500

    exercise 3.2 and made a change in header.js file for output

--- week-3/exercise-3.2.js
+++ week-3/exercise-3.2.js
@@ -1,3 +1,18 @@
+'use strict';
+/*
+============================================
+; Title:  Exercise 3.2
+; Author: Loren Wetzel
+; Date:   12 June 2019
+; Modified By:
+; Description: creating function to test for pattern matching
+;===========================================
+*/
+
+//header
+const header = require('../wetzel-header.js');
+console.log(header.display("Loren", "Wetzel", "Exercise 3.2"));
+
 /*
   Expected output:
 
@@ -14,3 +29,55 @@
   Bike and Bike do match!
   Four and Three do not match!
 */
+
+//start program
+
+//function to compare variables
+function match(x, y) {
+  if (x === y) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//functions to output match()
+function logMismatch(x, y) {
+  console.log(x + " and " + y + " do not match!");
+}
+
+function logMatch(x, y) {
+  console.log(x + " and " + y + " do match!");
+}
+
+//variables
+var test1 = "Truck", test2 = "Car",
+  test3 = "Bike", test4 = "Bike",
+  test5 = 4, test6 = 3;
+
+//output of match()
+console.log(match(test1, test2));
+console.log(match(test3, test4));
+console.log(match(test5, test6) + '\n');
+
+
+//output of if else blocks
+if (match(test1, test2)) {
+  logMatch(test1, test2);
+} else {
+  logMismatch(test1, test2);
+}
+
+if (match(test3, test4)) {
+  logMatch(test3, test4);
+} else {
+  logMismatch(test3, test4);
+}
+
+if (match(test5, test6)) {
+  logMatch(test5, test6);
+} else {
+  logMismatch(test5, test6);
+}
+
+//end program
